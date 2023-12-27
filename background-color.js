18ecf045d5401b43da2bd3c7812b34c034c1f3e9
juniorwebdev83 <juniorwebdev83@gmail.com>
2023-12-27 12:14:03 -0500

    nunca mais mexo

--- background-color.js
+++ background-color.js
@@ -1,10 +1,38 @@
-// background-color.js
+// Get the color picker container element and the label
+const colorPickerContainer = document.getElementById('color-picker-container');
+const colorPickerLabel = document.querySelector('#color-picker-container label');
 
-function changeBackgroundColor() {
-    // Get the selected color from the color picker
-    const colorPicker = document.getElementById('backgroundColor');
-    const selectedColor = colorPicker.value;
+let isDraggingColorPicker = false;
+let offsetXColorPicker, offsetYColorPicker;
 
-    // Change the background color of the body
-    document.body.style.backgroundColor = selectedColor;
+// Event listener for mouse down on the label
+colorPickerLabel.addEventListener('mousedown', handleMouseDown);
+
+// Function to handle mouse down event
+function handleMouseDown(e) {
+    isDraggingColorPicker = true;
+
+    // Calculate the offset from the click point to the top-left corner of the color picker container
+    offsetXColorPicker = e.clientX - colorPickerContainer.getBoundingClientRect().left;
+    offsetYColorPicker = e.clientY - colorPickerContainer.getBoundingClientRect().top;
+
+    // Change cursor style
+    colorPickerContainer.style.cursor = 'grabbing';
 }
+
+// Event listener for mouse move
+document.addEventListener('mousemove', (e) => {
+    if (!isDraggingColorPicker) return;
+
+    // Update the color picker container position based on mouse movement
+    colorPickerContainer.style.left = e.clientX - offsetXColorPicker + 'px';
+    colorPickerContainer.style.top = e.clientY - offsetYColorPicker + 'px';
+});
+
+// Event listener for mouse up
+document.addEventListener('mouseup', () => {
+    isDraggingColorPicker = false;
+
+    // Restore cursor style
+    colorPickerContainer.style.cursor = 'grab';
+});
--- encouraging-messages.js
+++ encouraging-messages.js
@@ -1,62 +1,61 @@
 // Encouraging messages array
-// Encouraging messages array
 const encouragingMessages = [
-    "you can see the training Material",
-    "Dont Worry About the time",
-    "Believe in Yourself!",
-    "Stay Positive!",
-    "Keep Going!",
-    "Don't Give Up!",
-    "you can see the training Material",
-    "You're Stronger Than You Think!",
-    "Every Step Counts!",
-    "Success is a Journey, Not a Destination!",
-    "Embrace the Journey!",
-    "Your Potential is Limitless!",
-    "Dream Big, Work Hard!",
-    "You're Resilient!",
-    "You're Making Progress!",
-    "You're Capable of Amazing Things!",
-    "Focus on Progress, Not Perfection!",
-    "Your Effort Matters!",
-    "You're on the Right Path!",
-    "Trust the Process!",
-    "Celebrate Small Victories!",
-    "Challenges Make You Stronger!",
-    "You Inspire Others!",
-    "Your Best is Yet to Come!",
-  ];
-  // Function to create and animate encouraging messages
+  "You can see the training material.",
+  "Don't worry about the time.",
+  "Believe in yourself!",
+  "Stay positive!",
+  "Keep going!",
+  "Don't give up!",
+  "You're stronger than you think!",
+  "Every step counts!",
+  "Success is a journey, not a destination!",
+  "Embrace the journey!",
+  "Your potential is limitless!",
+  "Dream big, work hard!",
+  "You're resilient!",
+  "You're making progress!",
+  "You're capable of amazing things!",
+  "Focus on progress, not perfection!",
+  "Your effort matters!",
+  "You're on the right path!",
+  "Trust the process!",
+  "Celebrate small victories!",
+  "Challenges make you stronger!",
+  "You inspire others!",
+  "Your best is yet to come!"
+];
+
+// Function to create and animate encouraging messages
 function createEncouragingMessage() {
-    // Create a new <div> element for the message
-    const messageElement = document.createElement('div');
-    messageElement.className = 'encouraging-message';
-  
-    // Randomly select a message from the array
-    const randomIndex = Math.floor(Math.random() * encouragingMessages.length);
-    const messageText = encouragingMessages[randomIndex];
-  
-    // Set the message text
-    messageElement.textContent = messageText;
-  
-    // Append the message element to the body
-    document.body.appendChild(messageElement);
-  
-    // Apply animation styles
-    messageElement.style.animation = 'drop 10s linear'; // Adjust the duration to slow down the animation
-  
-    // Remove the message element after the animation is complete
-    setTimeout(() => {
-      messageElement.remove();
-    }, 10000); // Adjust the timeout based on the animation duration
-  }
-  
-  // Function to continuously create encouraging messages
-  function startEncouragingMessages() {
-    setInterval(() => {
-      createEncouragingMessage();
-    }, 20000); // Adjust the interval based on how often you want a new message to appear
-  }
-  
-  // Start the encouraging messages
-  startEncouragingMessages();
+  // Create a new <div> element for the message
+  const messageElement = document.createElement('div');
+  messageElement.className = 'encouraging-message';
+
+  // Randomly select a message from the array
+  const randomIndex = Math.floor(Math.random() * encouragingMessages.length);
+  const messageText = encouragingMessages[randomIndex];
+
+  // Set the message text
+  messageElement.textContent = messageText;
+
+  // Append the message element to the body
+  document.body.appendChild(messageElement);
+
+  // Apply animation styles
+  messageElement.style.animation = 'drop 10s linear'; // Adjust the duration to slow down the animation
+
+  // Remove the message element after the animation is complete
+  setTimeout(() => {
+    messageElement.remove();
+  }, 10000); // Adjust the timeout based on the animation duration
+}
+
+// Function to continuously create encouraging messages
+function startEncouragingMessages(interval) {
+setInterval(() => {
+  createEncouragingMessage();
+}, interval);
+}
+
+// Start the encouraging messages every 10 minutes (600,000 milliseconds)
+startEncouragingMessages(600000);
